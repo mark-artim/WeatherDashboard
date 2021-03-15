3457c1226323c5035c2541d3954d4b14904ec3e0
Mark Artim <mark.artim@gmail.com>
2021-03-14 19:15:04 -0500

    Set Honolulu as default when page opens, added icons to 5day forecast, fixed issue with local stoarage for city

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,6 @@
             prevCities = JSON.parse(prevCitiesRaw);
         }
         for (let k = 0; k < prevCities.length; k++) {
-            // if(type=="add"){
-            //     k = prevCities.length-1;
-            //     console.log("type: "+ type + "k=" + k);
-            // };
             var cityDisplay = document.getElementById("cityList");
             let newListItem = document.createElement("div");
             newListItem.setAttribute("class","cityList");
@@ -31,7 +27,9 @@
 // GET LAT & LONG FROM DIRECT GEOCODING API
 
     function cityValidation(citySt) {
-        var citySt = document.getElementById('city').value;
+        // if(citySt == "") {
+            var citySt = document.getElementById('city').value;
+        // }
         console.log(citySt);
         var checkMe = citySt.split(",");
         console.log("citySt Length: "+checkMe.length);
@@ -61,19 +59,15 @@
 // ADD NEW SEARCH CITY TO LOCAL STORAGE
 
 function storeCity(lat,lon,city,state) {
-    var city = document.getElementById('city').value;
     console.log(city);
     var citiesRaw = localStorage.getItem("prevCities"); // get string of all data
     var citiesArray = []; // make an empty array
         if (!!citiesRaw) {
         citiesArray = JSON.parse(citiesRaw); //parse string into array
         }
-    citiesArray.unshift({ city: city, state: state, lat: lat, lon: lon}); // stick new city on teh end (TO-DO: Change to UNSHIFT)
-    // citiesArray.unshift({ city: city, state: state, lat: lat, lon: lon});
+    citiesArray.unshift({ city: city, state: state, lat: lat, lon: lon}); // stick new city on front end 
     localStorage.setItem("prevCities", JSON.stringify(citiesArray));
     showPrevCities("add");
-    // location.reload();
-    // showPrevCities();
     getWeather(lat,lon,city,state);
     };
 
@@ -96,7 +90,7 @@ function storeCity(lat,lon,city,state) {
 
   
       function drawWeather(d,city,state) {
-        console.log("Hello from drawWeather");
+        console.log("Hello from drawWeather. city: "+city+"state: "+state);
         console.log(d);
         document.getElementById('location').innerHTML = city; // + ", " + state;
         document.getElementById('description').innerHTML = "Currently " + d.current.weather[0].description;
@@ -120,12 +114,19 @@ function storeCity(lat,lon,city,state) {
         var weathDesc = d.daily[i].weather[0].description;
         var uvi = d.daily[i].uvi;
         var hum = d.daily[i].humidity;
+        var currentIcon = d.daily[i].weather[0].icon;
+        var currentIconURL = "http://openweathermap.org/img/wn/"+currentIcon+".png";
+        console.log("icon: "+currentIconURL);
+        // document.getElementById("nowIcon").src=currentIconURL;
         // console.log("desc: "+weathDesc+" uvi: "+uvi+" hum; "+hum);
         //get elements in place to attach stuff to
         var dailyContainer = document.getElementById("5day");
         var oneDay = document.createElement("div");
         oneDay.setAttribute("id","card"+i);
         oneDay.setAttribute("class","cardDate");
+        var iconE = document.createElement("img");
+        iconE.setAttribute("class","fiveDicon");
+        iconE.setAttribute("src",currentIconURL);
         var dateE = document.createElement("p");
         dateE.setAttribute("class","cardDate");
         var lowE = document.createElement("p");
@@ -134,10 +135,6 @@ function storeCity(lat,lon,city,state) {
         var uviE = document.createElement("p");
         var humE = document.createElement("p");
         oneDay.setAttribute("class", "fiveDayCard");
-        // lowE.setAttribute("class","fiveDayCard");
-        // highE.setAttribute("class","fiveDayCard");
-        // oneDay.setAttribute("style","width: 10rem")
-        // oneDay.textContent = date;
         dateE.textContent = date;
         lowE.textContent = "Low: "+ low.toFixed();
         highE.textContent = "High: "+ high.toFixed();
@@ -147,6 +144,7 @@ function storeCity(lat,lon,city,state) {
         dailyContainer.appendChild(oneDay);
         var card = document.getElementById("card"+i);
         card.appendChild(dateE);
+        card.appendChild(iconE);
         card.appendChild(weathDescE);
         card.appendChild(lowE);
         card.appendChild(highE);
@@ -158,6 +156,8 @@ function storeCity(lat,lon,city,state) {
 
 
 showPrevCities("all");
+// cityValidation("Honolulu,HI");
+getWeather("21.3069","-157.8583","Honolulu","HI")
 
 //  *** EVENT LISTENERS ***
 
@@ -168,12 +168,12 @@ cities.addEventListener("click", function(event){
     // clear current 5 day forecast
     $("#5day").empty();
     console.log(event);
-    var city = event.target.innerHTML
-    console.log("the city is: " + city)
+    // var city = event.target.innerHTML
     var lat = event.target.dataset.lat;
     var lon = event.target.dataset.lon;
     var city = event.target.dataset.city;
     var state = event.target.dataset.state;
+    console.log("From event listener the city is: " + city)
     console.log("lat: "+lat+" lon: "+lon)
     document.getElementsByClassName("fiveDayCard").innerHTML = "";
     getWeather(lat,lon,city,state);
